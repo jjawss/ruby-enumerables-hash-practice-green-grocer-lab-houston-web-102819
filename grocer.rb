5d36a4f8d019356fe4dab8fb442b1413578fdb8b
David Jaworski <jaws360@gmail.com>
2019-10-23 12:09:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,37 @@
+#def consolidate_cart(cart)
+  #cart_contents = cart.map do |item| 
+   #[item["AVOCADO"], item["AVOCADO"], item["KALE"]]
+  #end
+#p cart_contents
+#end
+
+#def consolidate_cart(cart)
+  #p cart.reduce {|x| x.values}
+#end
+#=>
+
+#THIS IS A GOOD EXAMPLE OF PRODUCING A NON REPEATING OUTPUT
+# supplies = []
+#holiday_supplies.each do |key, value|
+  #value.each do |key2, value2|
+    #supplies << value2
+  #end
+#end
+#This would give us a nice list each supply and would remove duplicates from the list:
+
+#lights
+#tree
+#champagne_glasses
+#BBQ
+#flags
+
+ 
+puts supplies.flatten.uniq 
+puts supplies.flatten.uniq
+
 def consolidate_cart(cart)
-  # code here
+  item_count = cart.count {|item| p item}
+  p item_count
 end
 
 def apply_coupons(cart, coupons)
@@ -13,3 +45,13 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+items = [
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"KALE"    => {:price => 3.00, :clearance => false}}
+]
+
+
+
+consolidate_cart(items)
